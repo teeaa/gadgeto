7b70ea363f1778d742ae920176056159e3d277ac
mcorbin <corbin.math@gmail.com>
2022-07-01 23:14:39 +0200

    Add support for more content-type
    Add support for urlencoded, multipart and XML request
    
    https://github.com/loopfz/gadgeto/pull/83

--- tonic/tonic_test.go
+++ tonic/tonic_test.go
@@ -190,9 +190,9 @@ func queryHandlerOld(c *gin.Context, in *queryInOld) (*queryInOld, error) {
 }
 
 type bodyIn struct {
-	Param                  string `json:"param" validate:"required"`
-	ParamOptional          string `json:"param-optional"`
-	ValidatedParamOptional string `json:"param-optional-validated" validate:"eq=|eq=foo|gt=10"`
+	Param                  string `json:"param" form:"param" validate:"required"`
+	ParamOptional          string `json:"param-optional" form:"param-optional"`
+	ValidatedParamOptional string `json:"param-optional-validated" form:"param-optional-validated" validate:"eq=|eq=foo|gt=10"`
 }
 
 func bodyHandler(c *gin.Context, in *bodyIn) (*bodyIn, error) {
